b86c4852a0149c6e920cbe72070e437905c291e3
Shilin Victor <chrono.monochrome@gmail.com>
2020-02-26 16:52:07 +0300

    smdk4412-common: camera: use camera.device@1.0-impl-legacy.exynos4
    Change-Id: I9c8e1471a4070662e5980291e04964e0a23cedcf

--- common.mk
+++ common.mk
@@ -113,9 +113,9 @@ PRODUCT_PACKAGES += \
     libMali \
     libEGL_mali \
     gCam \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl-exynos4 \
     android.hardware.camera.provider@2.4-service \
-    camera.device@1.0-impl-legacy \
+    camera.device@1.0-impl-exynos4 \
     android.hardware.sensors@1.0-impl \
     android.hardware.gnss@1.0-impl \
     libfimc \
