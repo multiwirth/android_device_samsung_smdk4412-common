53ea75feba585656b384c92d9d85fc9199071de5
Shilin Victor <chrono.monochrome@gmail.com>
2020-03-07 19:11:53 +0300

    smdk4412-common: vendor_prop: correct RIL lib name
    Change-Id: I6ff353a4671e74ebbfe58c8e204ab5a5d0dd71f1

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-	rild.libpath=/system/lib/libsec-ril.so \
+	rild.libpath=/system/lib/libsecril-shim.so \
 	ro.sf.lcd_density=320 \
 	ro.lcd_min_brightness=20 \
 	ro.egl.destroy_after_detach=true \
